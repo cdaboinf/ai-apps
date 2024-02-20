70025a195c04b341cad54f551405fc06714d788a
Carlos Daboin <carlos.daboin@ukg.com>
2024-02-20 09:31:42 -0500

    rag changes

--- open-ia-services/app/main.py
+++ open-ia-services/app/main.py
@@ -3,13 +3,15 @@ from fastapi import FastAPI
 
 #from .dependencies import get_query_token, get_token_header
 #from .internal import admin
-from routers import chat
+from app.routers import chat
+from app.routers import rag
 
 #app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
 
 
 app.include_router(chat.router)
+app.include_router(rag.router)
 
 @app.get("/")
 async def root():
---
+++ open-ia-services/app/routers/rag.py
@@ -0,0 +1,121 @@
+from fastapi import APIRouter, UploadFile
+from openai import OpenAI
+from pydantic import BaseModel
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain.document_loaders import DirectoryLoader
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+import pymongo
+
+
+router = APIRouter()
+
+class rag_request(BaseModel):
+    query: str = ''
+    session: str = ''
+
+OPENAI_API_KEY = ""
+ATLAS_VECTOR_SEARCH_INDEX_NAME = "doc_vector_search"
+MONGO_URI = ""
+
+@router.post("/rag/upload", tags=["AI RAG"])
+async def upload(file: UploadFile):  
+    try:
+        file_type = file.content_type
+        file_path = f"C:\\Users\\carlos.daboin\\Documents\\Dev\\Test-Apps\\python-ai\\ai-apps\\open-ia-services\\app\\routers\\files\\{file.filename}"
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    EMBEDDING_FIELD_NAME = "embedding"
+    client = pymongo.MongoClient(MONGO_URI)
+    db = client.CustomsVectorSearch
+    mdbcollection = db.SpecDocuments
+
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+    if file_type == "text/plain":
+        loader = DirectoryLoader("C:\\Users\\carlos.daboin\\Documents\\Dev\\Test-Apps\\python-ai\\ai-apps\\open-ia-services\\app\\routers\\files", glob=file.filename, show_progress=True)
+        data = loader.load()
+
+        x = MongoDBAtlasVectorSearch.from_documents(
+            documents=data, 
+            embedding=embeddings, 
+            collection=mdbcollection, 
+            index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME)
+
+    if file_type == "application/pdf":
+        loader = PyPDFLoader(f"C:\\Users\\carlos.daboin\\Documents\\Dev\\Test-Apps\\python-ai\\ai-apps\\open-ia-services\\app\\routers\\files\\{file.filename}")
+        data = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 50)
+        docs = text_splitter.split_documents(data)
+
+        # insert the documents in MongoDB Atlas Vector Search
+        x = MongoDBAtlasVectorSearch.from_documents(
+            documents=docs, 
+            embedding=embeddings, 
+            collection=mdbcollection, 
+            index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME)
+
+    return {"file": file.filename}
+
+@router.post("/rag/search", tags=["AI RAG"])
+async def search(request: rag_request):
+    # vector store object
+    client = pymongo.MongoClient(MONGO_URI)
+    db = client.CustomsVectorSearch
+    mdbcollection = db.SpecDocuments
+
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    vectorStore = MongoDBAtlasVectorSearch(mdbcollection, embeddings, index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME)
+
+    # Convert question to vector using OpenAI embeddings
+    # Perform Atlas Vector Search using Langchain's vectorStore
+    # similarity_search returns MongoDB documents most similar to the query    
+    docs = vectorStore.similarity_search(request.query, K=1)
+    as_output = docs[0].page_content
+
+    # Leveraging Atlas Vector Search paired with Langchain's QARetriever
+
+    # Define the LLM that we want to use -- note that this is the Language Generation Model and NOT an Embedding Model
+    # If it's not specified (for example like in the code below),
+    # then the default OpenAI model used in LangChain is OpenAI GPT-3.5-turbo, as of August 30, 2023
+    llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
+
+    # Get VectorStoreRetriever: Specifically, Retriever for MongoDB VectorStore.
+    # Implements _get_relevant_documents which retrieves documents relevant to a query.
+    retriever = vectorStore.as_retriever()
+
+    # Load "stuff" documents chain. Stuff documents chain takes a list of documents,
+    # inserts them all into a prompt and passes that prompt to an LLM.
+    qa = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=retriever)
+
+    # Execute the chain
+    retriever_output = qa.run(request.query)
+
+    return { "vector": as_output, "llm": retriever_output }
+
+@router.delete("/rag/clear", tags=["AI RAG"])
+async def clear_collection():
+    client = pymongo.MongoClient(MONGO_URI)
+    db = client.CustomsVectorSearch
+    db.SpecDocuments.delete_many({})
+
+def __get_specs_collection():
+    connection_string = "mongodb+srv://carlosdaboin:tg2yFWrEazgqve1V@clusterukgrapidsearch.v9zv1e4.mongodb.net/?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(connection_string)
+    db = client.CustomsVectorSearch
+    collection = db.SpecDocuments
+   
+    return collection
